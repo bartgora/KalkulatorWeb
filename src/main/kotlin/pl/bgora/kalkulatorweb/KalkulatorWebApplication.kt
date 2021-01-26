095cf342c8b30ce9cf01fc19f714de1cf65f6b85
bartlomiej-gora <bartlomiej.gora@gmail.com>
2021-01-26 23:00:37 +0100

    Initial commit

---
+++ src/main/kotlin/pl/bgora/kalkulatorweb/KalkulatorWebApplication.kt
@@ -0,0 +1,49 @@
+package pl.bgora.kalkulatorweb
+
+import com.github.bgora.rpnlibrary.Calculator
+import com.github.bgora.rpnlibrary.exceptions.RPNException
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@SpringBootApplication
+class KalkulatorWebApplication
+
+fun main(args: Array<String>) {
+    runApplication<KalkulatorWebApplication>(*args)
+}
+
+@RestController
+class Controller {
+
+    @GetMapping("/calculate/{input}")
+    fun calculate(@PathVariable input: String): Response {
+        val calculator = Calculator.createCalculator();
+        val result = calculator.calculate(replaceDiv(input)).toString()
+        return Response(input, result);
+    }
+
+}
+
+fun replaceDiv(input: String): String {
+    return input.replace("div", "/")
+}
+
+@ControllerAdvice
+class ControllerErrorHandler : ResponseEntityExceptionHandler() {
+
+
+    @ExceptionHandler(value = [(RPNException::class)])
+    fun handleApiException(ex: RPNException, request: WebRequest): ResponseEntity<ErrorResponse> {
+        val errorDetails = ErrorResponse(status = HttpStatus.BAD_REQUEST, errorMsg = ex.message)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails)
+    }
+}
+
+class Response(val input: String, val result: String)
+
+class ErrorResponse(val status: HttpStatus, val errorMsg: String?)
---
+++ src/test/kotlin/pl/bgora/kalkulatorweb/KalkulatorWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.bgora.kalkulatorweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KalkulatorWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
