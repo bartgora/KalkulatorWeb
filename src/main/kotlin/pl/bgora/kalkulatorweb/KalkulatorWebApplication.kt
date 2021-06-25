bc25d27d062fd485d7038ccc7154def1d85743cd
bgora <bartlomiej.gora@infobip.com>
2021-06-25 11:56:30 +0200

    Addded Open api spec for rest endpoints

--- src/main/kotlin/pl/bgora/kalkulatorweb/KalkulatorWebApplication.kt
+++ src/main/kotlin/pl/bgora/kalkulatorweb/KalkulatorWebApplication.kt
@@ -1,33 +1,58 @@
 package pl.bgora.kalkulatorweb
 
-import com.github.bgora.rpnlibrary.Calculator
 import com.github.bgora.rpnlibrary.exceptions.RPNException
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.path
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.ui.ReDocOptions
+import io.javalin.plugin.openapi.ui.SwaggerOptions
+import io.swagger.v3.oas.models.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import javax.swing.UIManager.get
 
 @SpringBootApplication
 class KalkulatorWebApplication
 
 fun main(args: Array<String>) {
+
     runApplication<KalkulatorWebApplication>(*args)
+
+    Javalin.create {
+        it.registerPlugin(getConfiguredOpenApiPlugin())
+        it.defaultContentType = "application/json"
+    }.routes {
+        path("calculate") {
+            get(CalculatorController::calculate)
+        }
+    }.start(7001)
+
+    println("Check out ReDoc docs at http://localhost:7001/redoc")
+    println("Check out Swagger UI docs at http://localhost:7001/swagger-ui")
 }
 
-@RestController
-class Controller {
-
-    @GetMapping("/calculate/{input}")
-    fun calculate(@PathVariable input: String): Response {
-        val calculator = Calculator.createCalculator();
-        val result = calculator.calculate(replaceDiv(input)).toString()
-        return Response(input, result);
-    }
-
-}
+fun getConfiguredOpenApiPlugin() = OpenApiPlugin(
+        OpenApiOptions(
+                Info().apply {
+                    version("1.0")
+                    description("User API")
+                }
+        ).apply {
+            path("/swagger-docs") // endpoint for OpenAPI json
+            swagger(SwaggerOptions("/swagger-ui")) // endpoint for swagger-ui
+            reDoc(ReDocOptions("/redoc")) // endpoint for redoc
+            defaultDocumentation { doc ->
+                doc.json("500", ErrorResponse::class.java)
+                doc.json("503", ErrorResponse::class.java)
+            }
+        })
 
 fun replaceDiv(input: String): String {
     return input.replace("div", "/")
@@ -35,7 +60,6 @@ fun replaceDiv(input: String): String {
 
 @ControllerAdvice
 class ControllerErrorHandler : ResponseEntityExceptionHandler() {
-
 
     @ExceptionHandler(value = [(RPNException::class)])
     fun handleApiException(ex: RPNException, request: WebRequest): ResponseEntity<ErrorResponse> {
