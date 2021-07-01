006424f8500d3b52595930c2057002585d1bc19b
bgora <bartlomiej.gora@infobip.com>
2021-07-01 15:19:23 +0200

    Cors problems

--- front-end/calculator-front/src/App.tsx
+++ front-end/calculator-front/src/App.tsx
@@ -3,31 +3,33 @@ import Info from "./components/Info";
 import Calculator from "./components/Calculator";
 import ResultList from "./components/ResultList";
 import { useEffect } from "react";
-import api from './api'
+import api from "./api";
 import { CalculationRecord } from "./collections";
-import { AxiosResponse } from "axios";
-
+import { AxiosRequestConfig, AxiosResponse } from "axios";
 
 interface Calculation {
-  records: CalculationRecord[] | null
+  records: CalculationRecord[] | null;
 }
 
 const App = (props: Calculation) => {
-
   const [state, setState] = useState<Calculation>();
 
   useEffect(() => {
     setState({
-      records : {} as CalculationRecord[]
-    });
-    
-  },[])
+      records: {} as CalculationRecord[],
+    } as Calculation);
+  }, []);
 
   const onCalculate = async (input: string) => {
-    const {data} = await api.post(input) as AxiosResponse;
-    const record = { input, data}
-
-  }
+    const { data } = (await api.post("/calculate/", null, {
+      params: {
+        input,
+      },
+    } as AxiosRequestConfig)) as AxiosResponse;
+    const result = data as String;
+    const record = { input, result } as CalculationRecord;
+    state?.records?.push(record);
+  };
   return (
     <div>
       <Info />
--- src/main/kotlin/pl/bgora/kalkulatorweb/CalculatorController.kt
+++ src/main/kotlin/pl/bgora/kalkulatorweb/CalculatorController.kt
@@ -1,16 +1,21 @@
 package pl.bgora.kalkulatorweb
 
 import com.github.bgora.rpnlibrary.Calculator
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin
 class CalculatorController {
 
     @CrossOrigin
     @GetMapping("/calculate/{input}")
-    fun calculate(@PathVariable input: String): Response {
+    fun calculate(@PathVariable input: String): ResponseEntity<Response> {
         val calculator = Calculator.createCalculator();
         val result = calculator.calculate(replaceDiv(input)).toString()
-        return Response(input, result);
+        val httHeaders = HttpHeaders()
+        httHeaders.add("Access-Control-Allow-Origin", "*")
+        return ResponseEntity.ok().headers(httHeaders).body(Response(input, result))
     }
 }
